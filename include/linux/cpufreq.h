7b84b2c0970cd51e119265e574a9e0dde38e3069
Ram <androlove11@gmail.com>
2017-05-16 21:47:13 +0530

    Add barry allen governor

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -472,7 +472,10 @@ extern struct cpufreq_governor cpufreq_gov_interactivex;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_interactivex)
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_CAFACTIVE) 
 extern struct cpufreq_governor cpufreq_gov_cafactive; 
-#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_cafactive) 
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_cafactive)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_BARRY_ALLEN)
+extern struct cpufreq_governor cpufreq_gov_barry_allen;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_barry_allen) 
 #endif
 
 /*********************************************************************
